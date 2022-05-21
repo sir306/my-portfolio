fe8c9c747d45da1781291d270512d82ec61cab45
sir306 <hardingnick00@gmail.com>
2022-05-22 02:17:41 +1200

    Update gamework.vue

--- pages/gamework.vue
+++ pages/gamework.vue
@@ -68,12 +68,12 @@ export default {
           githubLink: "https://github.com/sir306/ShooterProject",
         },
         {
-          title: "First Person Shooter Unreal Engine",
+          title: "First Ultimate Unreal Engine Project",
           description:
-            "This is my second Unreal Engine project, in this project you can see that it is comprised of C++ and UE Blueprints, this game is a single level prototype with multiple weapons and enemies. The enemies have varying speed, health, and damage amount to give the prototype some variety and depth. All firearms created stem from a base class I created and use line tracing from the crosshairs and the barrel tip and detect to see if there is blocking hits that are different to indicate that there is a possible actor in the way of the barrel and the target destination, the bullet trails are created using textures from the barrel to the end point, followed by an impact to give the illusion of bullet impacts. To keep the project simple and quick to deploy the enemies were given only melee weapons and simple AI tasks and movement to create a quick and fun prototype.",
+            "This project was completed by following a Udemy tutorial that I purchased and was able to learn a lot and was introduced to Unreal Engine where I fell in love with game programming and C++. Some of the things learned in this course were: Actors, AI, Animations, Blueprints, C++, Collisions, Game Pause, Game Save, Level Transitioning, Packaging, UMG and so much more. The concept of this game is melee combat system where the player can pick up different weapons and fight spiders and golems, the player can also collect potions and coins.",
 
-          githubTitle: "View Shooter Project Repo",
-          githubLink: "https://github.com/sir306/ShooterProject",
+          githubTitle: "View First UE Project Repo",
+          githubLink: "https://github.com/sir306/UltimateGameTutCleaned",
         },
       ],
     };
