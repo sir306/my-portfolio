f822f5d42298067fc60d1999c5bf830a88559ef1
sir306 <hardingnick00@gmail.com>
2022-07-20 23:01:23 +1200

    Update aboutme.vue
    tweaked the wording a little and added an employment status at the top

--- pages/aboutme.vue
+++ pages/aboutme.vue
@@ -18,6 +18,13 @@
         style="transform: translateY(60px)"
       />
       <h4
+        ref="employmentStatusTitle"
+        class="text-white text-sm md:text-md tracking-wider uppercase opacity-0 mb-3"
+        style="transform: translateY(30px)"
+      >
+        {{ this.currentEmployment }}
+      </h4>
+      <h4
         ref="shortTitle"
         class="text-white font-exo2 text-lg md:text-2xl uppercase opacity-0 mb-3"
         style="transform: translateY(30px)"
@@ -90,16 +97,15 @@ export default {
           paragraph1:
             "You may be wondering what I was doing prior to 2020 and work I may have been involved in, well to put simply I have been a wide range of careers since 2010 varying from different forms of laboring jobs, different security jobs such a site protection and hospitality security, my last job was in construction as a steel fixer which came to an end when I injured my knee.",
           paragraph2:
-            "While this injury was repaired and fit for duty I decided to go skiing and ended up epically wiping out and bending my knee 90 degrees in the wrong way (I know Ouch! Eww!) which kind of ended my journey in the world of a physically demanding work, so I was left with a broken leg for some time and had to figure out what I can do to make myself a career in something I’d enjoy doing, be able to provide for my family, and finally something that would stimulate me mentally. Which after evaluating the choices I had was an easy one to figure out as I had always wanted to get into coding.",
+            "After the injury I ended up requiring some surgery and committed to rebuilding my knee with lots of physio, once cleared and after some time I decided to go skiing and ended up epically wiping out and bending my knee in every other direction that it was supposed to go (I know Ouch! Eww!) which kind of ended my journey in the world of a physically demanding work i.e., steel fixing, so I was left with a broken leg for some time and had to figure out what I can do to make myself a career in something I’d enjoy doing, be able to provide for my family, and finally something that would stimulate me mentally. Which after evaluating the choices I had was an easy one to figure out as I had always wanted to get into coding.",
           paragraph3:
             "Some obstacles I had to over comer was that I had a big problem of imposter syndrome at the start of my learning journey and being nearly thirty at the time I decided to do this was a big hurdle for me to overcome as most of my peers would be fresh out of school. I didn’t let this deter me and pushed through knowing that my only challenge was me, once I overcame this and learnt that I can do this, I developed a real drive in bettering my knowledge in a variety of different coding languages and life skills that I can offer to employers, fellow developers and clients that will be an asset to them.",
           paragraph4:
             "What keeps me going and pushing is that I really do love learning this stuff and I just can’t get enough, sure there are other factors to this drive like expecting my first child in August has had a big impact in this but in short its easy to learn something you enjoy doing. I hope this wee blurb hasn’t bored you too much but felt anyone who wants to know me and check me out that these are some things you should know and want you to know, so thank you if you took time to read this.",
         },
       ],
-      currentEmployment: [
-        { employmentStatus: "Currently Seeking Employment or Internship" },
-      ],
+      currentEmployment:
+        "Employment Status: Currently Seeking Employment or Internship",
     };
   },
 
@@ -166,6 +172,12 @@ export default {
     });
 
     gsap.to(this.$refs.title, {
+      opacity: 1,
+      duration: 2,
+      y: 0,
+      ease: "expo",
+    });
+    gsap.to(this.$refs.employmentStatusTitle, {
       opacity: 1,
       duration: 2,
       y: 0,
