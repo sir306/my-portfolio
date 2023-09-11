260031032ba6b0e8062c5349b4967a271353c2be
Nicholas Harding <40708936+sir306@users.noreply.github.com>
2023-09-11 22:59:44 +1200

    Update gameprojects.js
    added internship project

--- data/gameprojects.js
+++ data/gameprojects.js
@@ -1,5 +1,15 @@
 export const GameProjects = [
     {
+      title: "Tsunami Evacuation Model Design at University of Canterbury (Internship)",
+      description:
+        "While this project is technically not a game, it fits this category well due to the technologies used and skills I learned developing this solution. This internship project was for the Civil and Natural Resource Engineering department at University of Canterbury, where I had to take 3D Google Earth images and convert them into a workable model to be used inside Unreal Engine with the goal to provide the researcher the ability to observe simulated human evacuation behavior in virtual reality in real time with a high-fidelity model. The solution I developed was an automated pipeline where the captured Google Earth 3D data can be processed inside of Blender and exported for use as an FBX file inside of Unreal Engine 5, with the goal to observe tsunami evacuations in virtual reality in real time, meaning the completed model had to run at a minimum 90 frames per second (to avoid issues such as motion sickness). Since completing this project, I have continued to develop the script further to produce a cleaner model and handle more errors that could occur on larger data sets and newer requirements of Unreal Engine’s Nanite feature to produce a visually correct model. Currently I am still making further changes to this project on a personal level and to make automated changes inside Unreal Engine and make further model optimizations in Blender, to provide users greater performance with the aim to allow lower end hardware to take advantage of this solution.",
+      youtubeLink: "https://www.youtube.com/playlist?list=PLnOWkX48zOuSgW9azekEe4PEHlMdNO_JX",
+      githubTitle: "View Google Earth Blender Automation Repo",
+      githubLink: "https://github.com/sir306/GoogleEarth_MapImport_CapMerge_TexPack_Automation",
+      languages: ["Python"],
+    },
+
+    {
       title: "First Person Shooter Unreal Engine",
       description:
         "This is my second Unreal Engine project, in this project you can see that it is comprised of C++ and UE Blueprints, this game is a single level prototype with multiple weapons and enemies. The enemies have varying speed, health, and damage amount to give the prototype some variety and depth. All firearms created stem from a base class I created and use line tracing from the crosshairs and the barrel tip and detect to see if there is blocking hits that are different to indicate that there is a possible actor in the way of the barrel and the target destination, the bullet trails are created using textures from the barrel to the end point, followed by an impact to give the illusion of bullet impacts. To keep the project simple and quick to deploy the enemies were given only melee weapons and simple AI tasks and movement to create a quick and fun prototype.",
